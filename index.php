4c8bbc67b9cad695fca5cb096f5c969e849fff11
hugofarfaday <ugolescargot0601@gmail.com>
2022-10-24 10:33:26 +0200

    SelectWhere

--- index.php
+++ index.php
@@ -1,22 +1,18 @@
 <?php
-
 $env = 'dev';
-$_ENV = json_decode(file_get_contents("src/Configs/" . $env . ".config.json"), true);
+$_ENV = json_decode(file_get_contents("src/Configs/" . $env . ".config.json"),
+true);
 $_ENV['env'] = $env;
-
 require_once 'autoload.php';
-
 use Helpers\HttpRequest;
 use Helpers\HttpResponse;
 use Services\DatabaseService;
 use Controllers\DatabaseController;
-
 $request = HttpRequest::instance();
 $tables = DatabaseService::getTables();
 if(empty($request->route) || !in_array($request->route[0], $tables)){
-    HttpResponse::exit();
+HttpResponse::exit();
 }
 $controller = new DatabaseController($request);
-//$result = $controller->execute();
-//HttpResponse::send(["data"=>$result]);
-HttpResponse::send(["message"=>"La table ".$request->route[0]." existe."]);
+$result = $controller->execute();
+HttpResponse::send(["data"=>$result]);
--- src/services/DatabaseService.php
+++ src/services/DatabaseService.php
@@ -62,4 +62,16 @@ public static function getTables() : array
         $tables = $resp->statment->fetchAll(PDO::FETCH_COLUMN);
         return $tables;
     } 
+
+/**
+* Retourne les lignes correspondant à la condition where
+*/
+public function selectWhere(string $where = "1", array $bind = []) : array
+{
+$sql = "SELECT * FROM $this->table WHERE $where;";
+        $resp = $this->query($sql, $bind);
+        $rows = $resp->statment->fetchAll(PDO::FETCH_CLASS);
+        return $rows;
+}
+
 }
