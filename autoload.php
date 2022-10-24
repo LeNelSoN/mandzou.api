2da8e16a2608324ec3e1e3338f0a6f9d9a409f3f
Patrick <bevis.patrick@gmail.com>
2022-10-24 08:26:45 +0200

    sprint 1 terminé

--- autoload.php
+++ autoload.php
@@ -2,10 +2,10 @@
 class Autoload{
 
     static function register(){
-        spl_autoload_register(array(__CLASS__, 'autoload'));
+        spl_autoload_register(array(__CLASS__, 'autoloader'));
     }
 
-    static function autoload($class){
-        require $class . '.php';
+    static function autoloader($class){
+        require 'src/' .$class . '.php';
     }
 }
